e4a6810a9d1db6c7fc68dca8cd071ded44a6143b
Thiago Proença <thiagoidro.11ti@gmail.com>
2022-08-21 03:22:09 -0300

    Correções em algumas classes + adição da classe de testes

--- codigo/TaxaDeRiqueza.java
+++ codigo/TaxaDeRiqueza.java
@@ -1,14 +1,16 @@
 public class TaxaDeRiqueza extends Espaco {
     // atributos
-    private static int taxa = 200;
+    private double taxa;
 
     // construtor
     public TaxaDeRiqueza(int posicao) {
+        this.taxa = 200;
+
         inicializaEspaco(posicao);
     }
 
     // getters e setters
-    private static double getTaxa() {
+    private double getTaxa() {
         return(taxa);
     }
 
@@ -23,8 +25,8 @@ public class TaxaDeRiqueza extends Espaco {
     }
 
     public double pagarTaxa() {
-        // o jogador paraga $200,00
+        // retorna o valor da taxa para que o jogador pague $200,00 ao banco
 
-        return(getTaxa());
+        return(this.getTaxa());
     }
 }
---
+++ codigo/main.java
@@ -0,0 +1,20 @@
+public class main {
+    // classe responsavel pelos testes da classe Espaco e suas sub-classes
+
+    /* TESTES - Espaco */
+    
+    /* TESTES - PontoDePartida */
+    
+    /* TESTES - VaParaCadeia */
+    
+    /* TESTES - Cadeia */
+    
+    /* TESTES - EspacoDeCarta */
+    
+    /* TESTES - TaxaDeRiqueza */
+    
+    /* TESTES - ImpostoDeRenda */
+    
+    /* TESTES - EstacionamentoGratis */
+    
+}
