46769e7978a120b32bea14973d80726746dc541e
Thiago Proença <thiagoidro.11ti@gmail.com>
2022-08-22 09:41:43 -0300

    Adicionei um novo teste para a classe 'ImpostoDeRenda'

--- codigo/EspacoTeste.java
+++ codigo/EspacoTeste.java
@@ -11,6 +11,8 @@ public class EspacoTeste {
         iniciaEspacoImpostoCorreto(5);
 
         // teste 2
+        ExecutarTeste("iniciaEspacoImpostoIncorreto", 2);
+        iniciaEspacoImpostoIncorreto(4);
     }
 
     public static void classeTestada(String nomeClasse) {
@@ -41,12 +43,38 @@ public class EspacoTeste {
         * os atributos 'nome' e 'posicao' devem ser "Imposto de Renda" e '5', respectivamente.
         * 
         * METODO TESTADO: contrutor - 'ImpostoDeRenda(int posicao)';
-        * para ser iniciado, é passado uma posicao (que sera a sua posicao em 'Tabuleiro')
+        * para ser iniciado, é passado uma posicao (que sera a sua posicao em 'Tabuleiro').
         * 
-        * RETORNO: constroi um objeto do tipo 'ImpostoDeRenda' para ser utilizado como 'Espaco' em 'Tabuleiro'
+        * RETORNO: constroi um objeto do tipo 'ImpostoDeRenda' para ser utilizado como 'Espaco' em 'Tabuleiro'.
         */
 
         public static void iniciaEspacoImpostoCorreto(int posicao) {
+            // saida esperada
+            System.out.println();
+
+            // teste
+            ImpostoDeRenda ir1 = new ImpostoDeRenda(posicao);
+
+            System.out.printf("\tPosicao: %d\n", ir1.getPosicao());
+            System.out.printf("\tNome: %s\n", ir1.getNome());
+        }
+
+        /* TESTE 2 - 'iniciaEspacoImpostoIncorreto()'
+        * 
+        * OBJETIVO: verificar o que um objeto 'ImpostoDeRenda' entrega quando iniciado incorretamente;
+        * o atributo 'nome' deve ser nulo, e o atributo 'posicao' deve ser '0' (zero).
+        * 
+        * METODO TESTADO: contrutor - 'ImpostoDeRenda(int posicao)';
+        * neste teste, inicializaremos ele com um numero diferente de '5'.
+        * 
+        * RETORNO: constroi um objeto do tipo 'ImpostoDeRenda' para ser utilizado como 'Espaco' em 'Tabuleiro'.
+        */
+
+        public static void iniciaEspacoImpostoIncorreto(int posicao) {
+            // saida esperada
+            System.out.println();
+
+            // teste
             ImpostoDeRenda ir1 = new ImpostoDeRenda(posicao);
 
             System.out.printf("\tPosicao: %d\n", ir1.getPosicao());
