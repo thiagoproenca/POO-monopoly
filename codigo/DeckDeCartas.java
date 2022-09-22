78ad5470ec67b89602b142f70ced1a5f26b55ea3
Thiago Proença <thiagoidro.11ti@gmail.com>
2022-09-22 11:24:47 -0300

    Mudanças finais no deck

--- codigo/DeckDeCartas.java
+++ codigo/DeckDeCartas.java
@@ -5,67 +5,43 @@
  * descrição.
  */
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class DeckDeCartas {
     // atributos
-    private Carta deck[];
+    private ArrayList<Carta> deck;
     private int topoDeck;
 
     // construtores
     public DeckDeCartas(int tipoDeDeck) {
-        // inicia um deck de cartas com 16 cartas
-        this.deck = new Carta[16];
-        deck = ordemCartaAlearotia(tipoDeDeck, 16);
+        for(int i=1; i<=16; i++) {
+            // carta de movimento
+            if(i < 8) {
+                deck.add(new CartaDeMovimento(tipoDeDeck, i));
+            }
+
+            // carta de dinheiro
+            else if(i < 16) {
+                deck.add(new CartaDeDinheiro(tipoDeDeck, i));
+            }
+
+            // carta 'va para cadeia'
+            else {
+                deck.add(new CartaVaParaCadeia(tipoDeDeck, i));
+            }
+        }
+        
+        // embaralha o deck
+        Collections.shuffle(deck);
 
         // o topo do deck comeca na primeira posicao
         this.topoDeck = 0;
     }
 
     // metodos especiais
-    private Carta[] ordemCartaAlearotia(int tipoDeDeck, int quantidade) {
-        /* metodo interno para facilitar a leitura do construtor
-         * define uma ordem aleatoria para o as cartas do deck
-        */
-
-        // inicializa um vetor que define a ordem dos turnos dos jogadores
-        Random random = new Random();
-        int[] ordemAleatoria = new int[quantidade];
-        Carta[] deckOrdemAleatoria = new Carta[quantidade];
-
-        // define uma ordem aleatoria
-        for(int i=quantidade-1; i>0; i--) {
-            int j = random.nextInt(i+1);
-
-            int temp = ordemAleatoria[i];
-            ordemAleatoria[i] = ordemAleatoria[j];
-            ordemAleatoria[j] = temp;
-        }
-
-        // atribui as cartas ao deck
-        for(int i=0; i<quantidade; i++) {
-            // cartas de movimento
-            if(ordemAleatoria[i] < 8) {
-                deckOrdemAleatoria[i] = new CartaDeMovimento(tipoDeDeck, ordemAleatoria[i]);
-            }
-
-            // cartas de dinheiro
-            else if(ordemAleatoria[i] < 16) {
-                deckOrdemAleatoria[i] = new CartaDeDinheiro(tipoDeDeck, ordemAleatoria[i]);
-            }
-
-            // carta 'VaParaCadeia'
-            else {
-                deckOrdemAleatoria[i] = new CartaVaParaCadeia(tipoDeDeck, ordemAleatoria[i]);
-            }
-        }
-
-        // retorna a ordem dos jogadores
-        return(deckOrdemAleatoria);
-    }
-
     private Carta getCarta(int posicaoNoDeck) {
-        return(this.deck[posicaoNoDeck]);
+        return(this.deck.get(posicaoNoDeck));
     }
 
     private int getTopoDeck() {
